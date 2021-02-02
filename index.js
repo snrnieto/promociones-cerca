1a24ce8447336a7e136a0a7d566f1883e1a87342
snrnieto <49009866+snrnieto@users.noreply.github.com>
2021-02-02 14:16:16 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+
+fetch('data.json')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    appendData(data);
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
+
+
+  function appendData(data) {
+    var mainContainer = document.getElementById("myData");
+    for (var i = 0; i < data.length; i++) {
+      html =`  
+        <div class="card">
+            <img src="${data[i].imagen}" class="promo-img"></img>
+            <div class="promo-content">
+                <h1 class="promo-title">${data[i].nombre}</h1>
+                <p class="promo-description">${data[i].descripcion}</p>
+                <button class="btn-solicitar">Solicitar promoción</button>
+            </div>
+        </div>
+    ` ;
+      mainContainer.innerHTML+=html;
+    }
+  }
+
