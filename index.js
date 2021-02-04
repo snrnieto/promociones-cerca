aba1e85bccfcfd142046dd0197b25b5806739916
snrnieto <49009866+snrnieto@users.noreply.github.com>
2021-02-04 11:16:36 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,51 @@
 
+var date = new Date();
+var semana = new Array(7);
+semana[0] = "Domingo";
+semana[1] = "Lunes";
+semana[2] = "Martes";
+semana[3] = "Miercoles";
+semana[4] = "Jueves";
+semana[5] = "Viernes";
+semana[6] = "Sábado";
+
+function shuffle(array) {
+  let counter = array.length;
+
+  // While there are elements in the array
+  while (counter > 0) {
+      // Pick a random index
+      let index = Math.floor(Math.random() * counter);
+
+      // Decrease counter by 1
+      counter--;
+
+      // And swap the last element with it
+      let temp = array[counter];
+      array[counter] = array[index];
+      array[index] = temp;
+  }
+
+  return array;
+}
+
 fetch('data.json')
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
-    appendData(data);
+    
+
+    var obj = JSON.parse(JSON.stringify(data))
+    promociones_hoy= obj.filter(item=>item.dias.includes(4));
+    promociones_hoy=shuffle(promociones_hoy)
+
+
+    var dia = semana[date.getDay()];
+    document.getElementById("titulo").innerHTML = "Promociones en Cali "+dia;
+
+
+    appendData(promociones_hoy);
   })
   .catch(function (err) {
     console.log(err);
@@ -27,7 +68,4 @@ fetch('data.json')
       mainContainer.innerHTML+=html;
     }
   }
-
-
-
 
