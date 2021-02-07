21ffdfedbed68105cf630fe2f8d3a08d2407a179
snrnieto <49009866+snrnieto@users.noreply.github.com>
2021-02-06 23:34:28 -0500

    Update index.js

--- index.js
+++ index.js
@@ -47,6 +47,32 @@ fetch('data.json')
 
 
     appendData(promociones_hoy);
+  
+      // Get the modal
+    var modal = document.getElementById("myModal");
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on the button, open the modal
+    btn.onclick = function() {
+      modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
   })
   .catch(function (err) {
     console.log(err);
@@ -62,7 +88,7 @@ fetch('data.json')
             <div class="promo-content">
                 <h1 class="promo-title">${data[i].nombre}</h1>
                 <p class="promo-description">${data[i].descripcion}</p>
-                <button class="btn-solicitar">Solicitar promoción</button>
+                <button id="myBtn" class="btn-solicitar">Solicitar promoción</button>
             </div>
         </div>
     ` ;
