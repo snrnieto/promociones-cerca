42d46462d1280c580b2bcd068d5a1de1659c7092
snrnieto <49009866+snrnieto@users.noreply.github.com>
2021-02-10 00:46:54 -0500

    Update index.js

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ fetch('data.json')
             <div class="promo-content">
                 <h1 class="promo-title">${data[i].nombre}</h1>
                 <p class="promo-description">${data[i].descripcion}</p>
-                <button onclick='alert(${data[i].nombre})' class="btn-solicitar">Solicitar promoción</button>
+                <button onclick=''`+alert(${data[i].nombre})+`'' class="btn-solicitar">Solicitar promoción</button>
             </div>
         </div>
     ` ;
